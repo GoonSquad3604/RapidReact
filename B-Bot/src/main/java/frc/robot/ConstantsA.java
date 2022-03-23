a5a392e925277ea6a95ff83c5821bb7475563214
test <test>
2022-03-23 18:09:57 -0400

    constants updated

--- B-Bot/src/main/java/frc/robot/ConstantsA.java
+++ B-Bot/src/main/java/frc/robot/ConstantsA.java
@@ -8,7 +8,7 @@ public final class ConstantsA {
     public static final int kTelescopeFullLeftA = 223840;
     public static final int kTelescopeOnBarA = 187683;
 
-    public static final double kShuttleForwardLeft = 151.543427;
+    public static final double kShuttleForwardLeft = 155.543427;
     public static final double kShuttleBackLeft = -170.207932;
 
 }
